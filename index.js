66b9a7c372cea3d3fc74cbe31da69c54361cf577
Garrett Breach <breachg1992@gmail.com>
2020-10-19 17:44:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHolidays() {
+  return ("Happy holidays!")
+}// write your code below!
+
+var name = "You"
+
+function happyHolidaysTo(name) {
+  return (`Happy holidays, ${name}!`)
+}
